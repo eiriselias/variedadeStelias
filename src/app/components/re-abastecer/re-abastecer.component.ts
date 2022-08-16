066d6df0712fdeb6202186fd31976d5f1ed89ac9
eiriselias <eiriselias@gmail.com>
2022-08-16 12:09:44 -0500

    se agrego formulario que permite agregar nuevos productos

--- src/app/components/re-abastecer/re-abastecer.component.ts
+++ src/app/components/re-abastecer/re-abastecer.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Producto } from 'src/app/models/productos.model';
+import { ProductosService } from 'src/app/services/productos.service';
 
 @Component({
   selector: 'app-re-abastecer',
@@ -7,9 +9,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReAbastecerComponent implements OnInit {
 
-  constructor() { }
+  codigo:number=0;
+  producto:string="";
+  cantidad:number=0;
+  nuevos:number=0;
+  novedad:boolean=false;
+  abastece: any;
+  productos:Producto[] = [];
+  
+
+  constructor(private produServi:ProductosService) { }
 
   ngOnInit(): void {
+    this.productos = this.produServi.productos;
   }
+  buscar(){
+    this.producto = this.productos[this.codigo].nombre;
+    this.cantidad = this.productos[this.codigo].cantidadExistente;
+  }
+  abastecer(){
+    if(this.productos[this.codigo].cantidadExistente < this.cantidad){
+      this.nuevos = this.cantidad - this.productos[this.codigo].cantidadExistente
+      this.productos[this.codigo].cantidadExistente = this.cantidad;
+      this.producto = "";
+      this.cantidad = 0;      
+      this.novedad=true;
+    }else{
+      alert("el abastecimiento debe contener un valor mayor al anterior");
+      this.novedad=false;
+    }
+  }
+
 
 }
